52734926ecbacade284a684972f9d3d95bbc9345
SagarOad <124421949+SagarOad@users.noreply.github.com>
2024-04-02 22:51:29 -0900

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import ProductCart from "@/components/productCart";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h2 className="">Hello World</h2>
+      <ProductCart />
+    </main>
+  );
+}
