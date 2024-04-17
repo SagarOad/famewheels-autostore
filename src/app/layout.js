b107854859e57d6f55c9c928cb5cdfb849ccdaa2
SagarOad <124421949+SagarOad@users.noreply.github.com>
2024-04-17 02:47:53 -0900

    New changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import "slick-carousel/slick/slick.css";
+// import "slick-carousel/slick/slick-theme.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="max-w-[1600px] mx-auto">{children}</div>
+        <div className="max-w-[1400px] mx-auto">{children}</div>
       </body>
     </html>
   );
